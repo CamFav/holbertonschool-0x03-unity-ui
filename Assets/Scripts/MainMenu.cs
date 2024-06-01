043205e1b140a1bfdda9fb1dbc067551f7cf1e91
Camille F <camillefvrl@gmail.com>
2024-06-01 15:59:59 +0200

    [T2] Added ColorBlind Mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Method to load the maze scene
+
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    // Method to load the Maze scene with or not ColorBlind Mode
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 255); // Orange
+            goalMat.color = Color.blue;
+        }
+
         SceneManager.LoadScene("maze");
     }
 
